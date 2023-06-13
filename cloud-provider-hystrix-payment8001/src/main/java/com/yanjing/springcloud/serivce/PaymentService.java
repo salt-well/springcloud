3c2d86165652dcda77a606f72eb7d5e3ba00a15e
salt-well <714403409@qq.com>
2023-06-13 16:41:00 +0800

    添加hystrix dashboard

--- cloud-provider-hystrix-payment8001/src/main/java/com/yanjing/springcloud/serivce/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yanjing/springcloud/serivce/PaymentService.java
@@ -1,8 +1,10 @@
 package com.yanjing.springcloud.serivce;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +22,7 @@ public class PaymentService
     }
 
     /**
-     * 超时访问，演示降级
+     * 1.超时访问，演示降级（服务降级）
      * 下面故意制造两个异常：
      *    1.  int age = 10/0; 计算异常
      *    2.  我们能接受3秒钟，它运行4秒钟，超时异常。
@@ -43,5 +45,35 @@ public class PaymentService
         return "/(ㄒoㄒ)/调用支付接口超时或异常：\t"+ "\t当前线程池名字" + Thread.currentThread().getName() + "\t 线程超时!!";
     }
 
+    /**
+     * 2.服务熔断
+     * 涉及到断路器的三个重要参数：快照时间窗、请求总数阀值、错误百分比阀值。
+     * 1：快照时间窗：断路器确定是否打开需要统计一些请求和错误数据，而统计的时间范围就是快照时间窗，默认为最近的10秒。
+     * 2：请求总数阀值：在快照时间窗内，必须满足请求总数阀值才有资格熔断。默认为20，意味着在10秒内，如果该hystrix命令的调用次数不足20次，即使所有的请求都超时或其他原因失败，断路器都不会打开。
+     * 3：错误百分比阀值：当请求总数在快照时间窗内超过了阀值，比如发生了30次调用，如果在这30次调用中，有15次发生了超时异常，也就是超过50%的错误百分比，在默认设定50%阀值情况下，这时候就会将断路器打开
+     */
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",
+    commandProperties = {
+            //开启断路器
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),
+            //请求阀值：10秒内10次
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
+            //快照时间窗,10秒内
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+            //错误百分比阀值：10秒内10次请求有60%错误，熔断打开
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),
+   })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if(id < 0 ){
+            throw  new RuntimeException("*******id 不能为负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName()+"/t"+"调用成功,流水号："+serialNumber;
+    }
+
+
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id 不能负数，请稍后再试，/(ㄒoㄒ)/~~   id: " +id;
+    }
 }
 
